2fa5bf50a17f70243c61a2ed438ab8c79ed762c8
samuveljohnson1416 <samuveljohnson14102005@gmail.com>
2026-03-08 16:57:08 +0530

    feat:added crcrepo & stdrepo

---
+++ src/main/java/com/institute/scrs/repository/StudentRepo.java
@@ -0,0 +1,14 @@
+package com.institute.scrs.repository;
+
+import com.institute.scrs.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface StudentRepo extends JpaRepository<Student, Long> {
+
+    List<Student> findByStdNameContainingIgnoreCase(String stdName);
+
+    List<Student> findByCrs_CrsName(String crsName);
+}
