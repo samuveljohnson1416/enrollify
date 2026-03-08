0097f11f23aa35c617fb59509cedf8a2fae9e763
samuveljohnson1416 <samuveljohnson14102005@gmail.com>
2026-03-08 15:48:12 +0530

    project initialization

---
+++ src/main/java/com/institute/scrs/ScrsApplication.java
@@ -0,0 +1,13 @@
+package com.institute.scrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/institute/scrs/ScrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.institute.scrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
